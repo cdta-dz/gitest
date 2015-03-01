8d934269abe15b62c29d110a7a98c361a326161e
Mabrouk BOUMARAF <mboumaraf@cdta.dz>
2015-03-01 16:04:34 +0100

    commit all

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="en" xmlns="http://www.w3.org/1999/xhtml">
 <head>
 	<meta charset="utf-8"/>
-	<title>titre</title>
+	<title>titre 1</title>
 	<link href=".css" rel="stylesheet"/>
 	<script src=".js"></script>
 	<!--[if lt IE 9]>
